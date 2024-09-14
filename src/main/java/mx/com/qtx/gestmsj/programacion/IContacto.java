201117cbedc45278bca717aa62a45ab340bc7fc7
tribilin <rogelioisc@gmail.com>
2024-09-13 20:09:27 -0600

    Se agregaron los argumentos a los metodos set

--- src/main/java/mx/com/qtx/gestmsj/programacion/IContacto.java
+++ src/main/java/mx/com/qtx/gestmsj/programacion/IContacto.java
@@ -3,9 +3,9 @@ package mx.com.qtx.gestmsj.programacion;
 import java.util.List;
 
 public interface IContacto {
-	public void setNombreContacto();
-	public void setCorreoContacto();
-	public void setTelefonoContacto();
+	public void setNombreContacto(String nombre);
+	public void setCorreoContacto(String correo);
+	public void setTelefonoContacto(String telefono);
 	public String getNombreContacto();
 	public String getTelefonoContacto();
 	public String getCorreoContacto();
