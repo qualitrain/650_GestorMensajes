89c35f6957f5bb1f628d26ff6a3cd54dfea3e63d
RoyerBebe <tiburon_roge@hotmail.com>
2024-09-13 20:28:24 -0600

    Nuevo Pull Request, hehehehe Req03 rogelio (#8)
    * Metodos de la Interfaz Contacto
    
    * Se corrigieron los metodos de IContacto
    
    * Se agregaron los argumentos a los metodos set
    
    * Hola de nuevo, mando los metodos corregidos de IContacto, se agregaron parámetros. jeje :) (#7)
    
    * programacion entidades
    
    * Se sube requerimiento 04 (#4)
    
    * Se sube requerimiento 04
    
    Se crean interfaces y paquete de pruebas vacios
    
    * Se eliminan clases de implementacion y de pruebas
    
    ---------
    
    Co-authored-by: carlos <micuenta@midominio.com.mx>
    
    * Entidad contacto (#5)
    
    ---------
    
    Co-authored-by: yooo <micuenta@gmail.com.mx>
    Co-authored-by: Vicente MARQUEZ HERNANDEZ <vicente_mh@live.com.mx>
    Co-authored-by: carlosepeza <carlose.lopez.ubam@gmail.com>
    Co-authored-by: carlos <micuenta@midominio.com.mx>
    Co-authored-by: rlopeza1968 <ramon.lopez.factor@gmail.com>
    
    ---------
    
    Co-authored-by: tribilin <rogelioisc@gmail.com>
    Co-authored-by: yooo <micuenta@gmail.com.mx>
    Co-authored-by: Vicente MARQUEZ HERNANDEZ <vicente_mh@live.com.mx>
    Co-authored-by: carlosepeza <carlose.lopez.ubam@gmail.com>
    Co-authored-by: carlos <micuenta@midominio.com.mx>
    Co-authored-by: rlopeza1968 <ramon.lopez.factor@gmail.com>

--- src/main/java/mx/com/qtx/gestmsj/programacion/IContacto.java
+++ src/main/java/mx/com/qtx/gestmsj/programacion/IContacto.java
@@ -1,5 +1,12 @@
 package mx.com.qtx.gestmsj.programacion;
 
-public interface IContacto {
+import java.util.List;
 
+public interface IContacto {
+	public void setNombreContacto(String nombre);
+	public void setCorreoContacto(String correo);
+	public void setTelefonoContacto(String telefono);
+	public String getNombreContacto();
+	public String getTelefonoContacto();
+	public String getCorreoContacto();
 }
