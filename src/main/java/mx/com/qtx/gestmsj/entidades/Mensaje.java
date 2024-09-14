92aac28f3c1cf89b9bd8be8386fbbfad0ae0a1a8
RoyerBebe <tiburon_roge@hotmail.com>
2024-09-13 20:22:40 -0600

    Hola de nuevo, mando los metodos corregidos de IContacto, se agregaron parámetros. jeje :) (#7)
    * programacion entidades
    
    * Se sube requerimiento 04 (#4)
    
    * Se sube requerimiento 04
    
    Se crean interfaces y paquete de pruebas vacios
    
    * Se eliminan clases de implementacion y de pruebas
    
    ---------
    
    Co-authored-by: carlos <micuenta@midominio.com.mx>
    
    * Entidad contacto (#5)
    
    ---------
    
    Co-authored-by: yooo <micuenta@gmail.com.mx>
    Co-authored-by: Vicente MARQUEZ HERNANDEZ <vicente_mh@live.com.mx>
    Co-authored-by: carlosepeza <carlose.lopez.ubam@gmail.com>
    Co-authored-by: carlos <micuenta@midominio.com.mx>
    Co-authored-by: rlopeza1968 <ramon.lopez.factor@gmail.com>

---
+++ src/main/java/mx/com/qtx/gestmsj/entidades/Mensaje.java
@@ -0,0 +1,40 @@
+package mx.com.qtx.gestmsj.entidades;
+
+public class Mensaje {
+	
+	private int idMensaje;
+	private String encabezado;
+	private String cuerpoMensaje;
+	
+	public Mensaje(int idMensaje, String encabezado, String cuerpoMensaje) {
+		super();
+		this.idMensaje = idMensaje;
+		this.encabezado = encabezado;
+		this.cuerpoMensaje = cuerpoMensaje;
+	}
+	public int getIdMensaje() {
+		return idMensaje;
+	}
+	public void setIdMensaje(int idMensaje) {
+		this.idMensaje = idMensaje;
+	}
+	public String getEncabezado() {
+		return encabezado;
+	}
+	public void setEncabezado(String encabezado) {
+		this.encabezado = encabezado;
+	}
+	public String getCuerpoMensaje() {
+		return cuerpoMensaje;
+	}
+	public void setCuerpoMensaje(String cuerpoMensaje) {
+		this.cuerpoMensaje = cuerpoMensaje;
+	}
+	@Override
+	public String toString() {
+		return "Mensaje [idMensaje=" + idMensaje + ", encabezado=" + encabezado + ", cuerpoMensaje=" + cuerpoMensaje
+				+ "]";
+	}
+
+
+}
