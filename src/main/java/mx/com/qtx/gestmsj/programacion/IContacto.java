9baed849a481e4cda4c98642979a0eb4c3a44d17
tribilin <rogelioisc@gmail.com>
2024-09-13 19:35:05 -0600

    Metodos de la Interfaz Contacto

--- src/main/java/mx/com/qtx/gestmsj/programacion/IContacto.java
+++ src/main/java/mx/com/qtx/gestmsj/programacion/IContacto.java
@@ -1,5 +1,8 @@
 package mx.com.qtx.gestmsj.programacion;
 
-public interface IContacto {
+import java.util.List;
 
+public interface IContacto {
+	public void setDatosContacto(String idContacto, String nombre, String telefono );
+	public String getDatosContacto();
 }
