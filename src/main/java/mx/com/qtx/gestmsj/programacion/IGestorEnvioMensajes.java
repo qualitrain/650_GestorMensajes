21df95c31e656bc0a08f549fb0bdac8ed6d3585e
TuPadre <tupadre@entuhogar.com.mx>
2024-09-13 19:19:04 -0600

    Primer Prueba de oi nterface

--- src/main/java/mx/com/qtx/gestmsj/programacion/IGestorEnvioMensajes.java
+++ src/main/java/mx/com/qtx/gestmsj/programacion/IGestorEnvioMensajes.java
@@ -2,4 +2,11 @@ package mx.com.qtx.gestmsj.programacion;
 
 public interface IGestorEnvioMensajes {
 
+    void enviarMensaje(String destinatario, String mensaje);
+    
+    String obtenerMensaje(int mensajeId);
+    
+    boolean eliminarMensaje(int mensajeId);
+    
+
 }
