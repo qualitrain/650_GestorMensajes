b2c6f77565f924e058d6a0723a3bd5fa8e03e89b
TuPadre <tupadre@entuhogar.com.mx>
2024-09-13 19:25:47 -0600

    Primer revision por super Master Blaster se corrige variable y nomenclatura Get

--- src/main/java/mx/com/qtx/gestmsj/programacion/IGestorEnvioMensajes.java
+++ src/main/java/mx/com/qtx/gestmsj/programacion/IGestorEnvioMensajes.java
@@ -3,10 +3,7 @@ package mx.com.qtx.gestmsj.programacion;
 public interface IGestorEnvioMensajes {
 
     void enviarMensaje(String destinatario, String mensaje);
-    
-    String obtenerMensaje(int mensajeId);
-    
-    boolean eliminarMensaje(int mensajeId);
-    
-
+    String getMensaje(long mensajeId);
+    boolean eliminarMensaje(long mensajeId);
+ 
 }
