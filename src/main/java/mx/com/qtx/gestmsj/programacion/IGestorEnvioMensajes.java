9d8f1bdd16b9424ef5f504a7417c85340720d587
TuPadre <tupadre@entuhogar.com.mx>
2024-09-17 16:49:41 -0600

    Prueba de permisos en Branch

--- src/main/java/mx/com/qtx/gestmsj/programacion/IGestorEnvioMensajes.java
+++ src/main/java/mx/com/qtx/gestmsj/programacion/IGestorEnvioMensajes.java
@@ -5,5 +5,5 @@ public interface IGestorEnvioMensajes {
     void enviarMensaje(String destinatario, String mensaje);
     String getMensaje(long mensajeId);
     boolean eliminarMensaje(long mensajeId);
- 
+    //Hola prueba de permisos 
 }
